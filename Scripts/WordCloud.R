7592a2fe136a7c9959c7ec840a535856ab419a95
CHOUAIB HAMEK <chouaib.hamek@student.oulu.fi>
2017-10-27 20:22:18 +0300

    Update wordCloud script
    Add colored graphs and save the clouds to png files for the three source

--- Scripts/WordCloud.R
+++ Scripts/WordCloud.R
@@ -1,31 +1,50 @@
 rm(list = ls())
-#setwd('C:/Users/mmantyla/Dropbox/Teach/2017_Trends')
-#setwd('D:/Dropbox/Teach/2017_Trends')
-load ("my_articles_clean.RData")
-load ("twitter_articles_clean.RData")
-load ("stackoverflow_articles_clean.RData")
-
-
-my_text <- paste (my_articles$Title, my_articles$Abstract_clean)
-
-#library(text2vec)
+#install.packages("wordcloud", dependencies = TRUE)
 library (tm)
 library(magrittr)
-#This line could also be part of getting data. 
-removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]","",x)
-my_text = my_text %>% removeNumbers %>% removeSpecialChars %>% tolower # = tolower(removePunctuation(removeNumbers(my_text)))
-
-
-#install.packages("wordcloud", dependencies = TRUE)
 library(wordcloud)
-wordcloud(my_text, max.words = 50, random.order = FALSE)
+library(RColorBrewer)
 
-#Since our data was "TITLE-ABS-KEY(\"agile software development process\")"
-#it makes sense to remove the works required in the query and re-plot
-my_text = removeWords(my_text, c ("agile", "software", "development", "process", "processes"))
+# Loading the three sources of Data
+load ("C:/Users/Chouaib/Desktop/FinalReport/Data/my_scopus_ci_data.RData")
+scopus_articles = my_articles
+load ("C:/Users/Chouaib/Desktop/FinalReport/Data/my_STO_ci_data.RData")
+sto_articles = my_articles
+load ("C:/Users/Chouaib/Desktop/FinalReport/Data/my_twitter_ci_data.RData")
+twitter_articles = my_articles
 
-my_text = removeWords(my_text, c ("robot", "framework", "development", "process", "processes"))
-my_text = removeWords(my_text, c ("test", "automation", "twitter", "http", "com"))
+stopword_list = c(stopwords("english"), "microservice", "microservices", "service", "services", "use", "data", 
+                  "application", "system", "systems", "architecture", "applications", "paper", "can", "based", 
+                  "approach", "provide", "based", "using", "software", "twitter", "com", "architecture", "via", 
+                  "new", "pic", "need", "want", "like", "one", "will", "development", "model", "models", "also", 
+                  "architectures", "used", "present", "environment", "way", "app", "get", "project", "request", 
+                  "different", "code", "also", "create", "great", "however", "run", "running", "multiple", "know", 
+                  "now", "trying", "example", "micro", "two", "just", "file", "call", "make", "access", "ive", 
+                  "another", "best", "question", "message", "don't", "dont", "build", "work", "thanks", "following")
 
-#A good is to remove more words that we do not care about 
-my_text = removeWords(my_text, c ("can", "used", "use", "paper", "approach", "however", "new", "study", "using"))
+scopus_text <- paste (scopus_articles$Title, scopus_articles$Abstract_clean)
+sto_text <- paste (sto_articles$Title, sto_articles$Abstract_clean)
+twitter_text <- paste (twitter_articles$Title, twitter_articles$Abstract_clean)
+
+
+removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]","",x)
+
+scopus_text = scopus_text %>% removeNumbers %>% removeSpecialChars %>% tolower
+sto_text = sto_text %>% removeNumbers %>% removeSpecialChars %>% tolower
+twitter_text = twitter_text %>% removeNumbers %>% removeSpecialChars %>% tolower
+
+scopus_text =  removeWords(scopus_text,stopword_list)
+sto_text =  removeWords(sto_text,stopword_list)
+twitter_text =  removeWords(twitter_text,stopword_list)
+
+png("C:/Users/Chouaib/Desktop/FinalReport/Graphs/Scopus_word_cloud.png")
+wordcloud(scopus_text, scale=c(8,.3), max.words = 100, random.order = FALSE, colors=brewer.pal(8,"Dark2"))
+dev.off()
+
+png("C:/Users/Chouaib/Desktop/FinalReport/Graphs/STO_word_cloud.png")
+wordcloud(sto_text, scale=c(8,.3), max.words = 100, random.order = FALSE, colors=brewer.pal(9, "Dark2"))
+dev.off()
+
+png("C:/Users/Chouaib/Desktop/FinalReport/Graphs/Twitter_word_cloud.png")
+wordcloud(twitter_text, scale=c(8,.3), max.words = 100, random.order = FALSE, colors=brewer.pal(9, "Dark2"))
+dev.off()
