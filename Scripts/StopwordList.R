b14b38ae0b6d558da20bf135846985717b16bfd3
CHOUAIB HAMEK <chouaib.hamek@student.oulu.fi>
2017-10-28 00:07:11 +0300

    Add stopword list in a separate file

---
+++ Scripts/StopwordList.R
@@ -0,0 +1,9 @@
+final_stopword_list = c(stopwords("english"), "microservice", "microservices", "service", "services", "use", "data", 
+                        "application", "system", "systems", "architecture", "applications", "paper", "can", "based", 
+                        "approach", "provide", "based", "using", "software", "twitter", "com", "architecture", "via", 
+                        "new", "pic", "need", "want", "like", "one", "will", "development", "model", "models", "also", 
+                        "architectures", "used", "present", "environment", "way", "app", "get", "project", "request", 
+                        "different", "code", "also", "create", "great", "however", "run", "running", "multiple", "know", 
+                        "now", "trying", "example", "micro", "two", "just", "file", "call", "make", "access", "ive", 
+                        "another", "best", "question", "message", "don't", "dont", "build", "work", "thanks", "following",
+                        "implementation", "enabling", "towards", "basic", "event", "projects", "check", "part", "aware")
